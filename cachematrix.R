6883c67bb8cf6894003e3e905c4a788674fee23b
SUNIL KUMAR CHINNAMGARI <sunil_sixsigma@yahoo.com>
2014-06-21 22:44:15 +0530

    New changes done to functions makeCacheMatrix and cacheSolve as per instructions provided in the Assignment2 of R Programming course on Coursera.org

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix creates a special matrix that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+matinv <<- solve(x)
+  set <- function(y) {
+    x <<- y
+    matinv <<- NULL
+  }
+  get <- function() x
+  setmatinv <- function(matinv) matinv <<- matinv
+  getmatinv <- function() matinv
+  list(set = set, get = get,
+       setmatinv = setmatinv,
+       getmatinv = getmatinv)
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has
+## already been calculated (and the matrix has not changed), then cacheSolve will retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+         matinv <- x$getmatinv()
+  if (!is.null(matinv)) {
+    message("getting cached data")
+    return(matinv)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmatinv(matinv)
+  matinv
 }
